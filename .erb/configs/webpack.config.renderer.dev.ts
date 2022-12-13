bf513437f1ebc628cd017617492f67746d2fa7a2
MiniOcean404 <1037306928@qq.com>
2022-12-13 11:01:53 +0800

    fix: 报错问题

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -40,8 +40,7 @@ const configuration: webpack.Configuration = {
 
   mode: 'development',
 
-  // target: ['web', 'electron-renderer'],
-  target: 'electron-renderer',
+  target: ['web', 'electron-renderer'],
 
   entry: [
     `webpack-dev-server/client?http://localhost:${port}/dist`,
@@ -53,9 +52,9 @@ const configuration: webpack.Configuration = {
     path: webpackPaths.distRendererPath,
     publicPath: '/',
     filename: 'renderer.dev.js',
-    // library: {
-    //   type: 'umd',
-    // },
+    library: {
+      type: 'umd',
+    },
   },
 
   module: {
